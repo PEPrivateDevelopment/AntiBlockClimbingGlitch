e7af703abf9422e06e8983d2c2cc0e25c1144d14
Fernet <110495840+fernet1911@users.noreply.github.com>
2023-04-02 17:43:34 +0200

    Accurately TP the player back (fixes glitching through blocks higher than 1)

--- src/main/java/io/github/fernet1911/antiblockclimbingglitch/listeners/BlockPlaceListener.java
+++ src/main/java/io/github/fernet1911/antiblockclimbingglitch/listeners/BlockPlaceListener.java
@@ -3,6 +3,7 @@ package io.github.fernet1911.antiblockclimbingglitch.listeners;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Slab;
 import org.bukkit.entity.Player;
@@ -10,6 +11,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.util.BoundingBox;
 
 public class BlockPlaceListener implements Listener {
 
@@ -28,14 +30,32 @@ public class BlockPlaceListener implements Listener {
             }
 
             /*
-            Teleport the player back 1 block below if the block was placed on the same (or lower) Y of the player
+            Teleport the player back 1 block below if the Y of the placed block is <= of the Y of the player
             and the distance between the X and Z of the placed block is <= 0.3 of the X and Z of the player
              */
             double xDiff = block.getX() - location.getX();
             double zDiff = block.getZ() - location.getZ();
-            if ((xDiff <= 0.3 && xDiff >= -1.3) && (zDiff <= 0.3 && zDiff >= -1.3) && location.getBlockY() >= block.getY() && location.getBlock().isPassable()) {
-                location.add(0, -1, 0);
-                if (location.getBlock().isPassable()) {
+            Block firstBlock = location.getBlock();
+            if ((xDiff <= 0.3 && xDiff >= -1.3) && (zDiff <= 0.3 && zDiff >= -1.3) && location.getBlockY() >= block.getY() && firstBlock.isPassable()) {
+                Block secondBlock = firstBlock.getRelative(BlockFace.DOWN);
+                if (secondBlock.isPassable()) {
+                    Block thirdBlock = secondBlock.getRelative(BlockFace.DOWN);
+
+                    // Get the height of the third block to TP the player to
+                    double y = 0;
+                    for (BoundingBox boundingBox : thirdBlock.getCollisionShape().getBoundingBoxes()) {
+                        double maxY = boundingBox.getMaxY();
+                        if (maxY > y) {
+                            y = maxY;
+                        }
+                    }
+                    // Set a default height of 1 for passable blocks like AIR
+                    if (y == 0) {
+                        y = 1;
+                    }
+
+                    // TP the player to the new location
+                    location.setY(thirdBlock.getY() + y);
                     player.teleport(location);
                 }
             }
